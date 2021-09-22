cd98e62c0fcf24d19085c5a94fa990db73ccb65e
HP <HP@LAPTOP-SMC8HIAI>
2021-09-22 11:21:27 +0530

    sur

--- automationqsp/src/qsp/RunTimePolymorphismLaunchBrowser.java
+++ automationqsp/src/qsp/RunTimePolymorphismLaunchBrowser.java
@@ -7,38 +7,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class RunTimePolymorphismLaunchBrowser {
 
+public class RunTimePolymorphismLaunchBrowser {
+	static WebDriver driver;
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the browser name that you want to open either chrome or firefox");
+		System.out.println("Enter the Browser name either chrome or firefox");
 		String browserValue = sc.nextLine();
-
 		if(browserValue.equalsIgnoreCase("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");//to avoid IllegalStateException
-			driver = new ChromeDriver();//to open chromeBrowser
+			System.setProperty("webdriver0.chrome.driver","./drivers/chromedriver.exe");//to avoid IllegalStateException		
+			driver=new ChromeDriver();//open chromeBrowser
+			Thread.sleep(6000);//delay of 6 seconds
 			driver.manage().window().maximize();//maximize the browser
-			driver.get("https://www.google.com");//launch the web application
-			driver.close();//Close the browser
-			
+			driver.close();//close the browser
 		}
-		
+
 		else if(browserValue.equalsIgnoreCase("firefox"))
 		{
-			System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");//to avoid IllegalStateException
-			driver = new FirefoxDriver();//open firefox
-			driver.manage().window().maximize();//maximize the browser
-			driver.get("https://www.google.com");//launch the web application
+			System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+			driver=new FirefoxDriver();
+			Thread.sleep(6000);
+			driver.manage().window().maximize();
 			driver.close();
+
 		}
 		else
 		{
-			System.out.println("enter the valid input");
+			System.out.println("enter the valid choice");
 		}
-		
-		
 	}
 }
