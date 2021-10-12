3c32b32f60a9e225a18f65c787f2e10cb0985187
HP <HP@LAPTOP-SMC8HIAI>
2021-10-12 12:10:45 +0530

    new commit 12

--- automationqsp/src/handlingTheDropdown/SelectTheOptionByValue.java
+++ automationqsp/src/handlingTheDropdown/SelectTheOptionByValue.java
@@ -1,25 +1,25 @@
-package webElementMethods;
+package handlingTheDropdown;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class SubmitMethod {
-
+public class SelectTheOptionByValue {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");                                                                                            
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("https://www.instagram.com");
-		driver.findElement(By.xpath("//input[contains(@name,'user')]")).sendKeys("surajsurajsuraj");
-		driver.findElement(By.xpath("//input[contains(@name,'pass')]")).sendKeys("sawantsawantsawant");
-		driver.findElement(By.xpath("//button[contains(@class,'L3NKy')]")).submit();
-
+		driver.get("file:///C:/Users/HP/Desktop/htmldocswcsm5a/singleSelectDropdown.html");
+		Thread.sleep(8000);
+		WebElement dropdownElement = driver.findElement(By.id("menu"));
 		
+		Select sel = new Select(dropdownElement);
+		sel.selectByValue("2");
 	}
 
 }
